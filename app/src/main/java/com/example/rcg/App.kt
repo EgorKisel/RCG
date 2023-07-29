a4f1337e0f6eea73234838855d4f98c5c46bcc42
Egor Kisel <kisel1989@mail.ru>
2023-07-29 13:57:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rcg/App.kt
@@ -0,0 +1,10 @@
+package com.example.rcg
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/example/rcg/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rcg.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.rcg.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
